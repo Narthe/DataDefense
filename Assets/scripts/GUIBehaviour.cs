851a9b0fc80d914c3d2501a6f61f79584d8a5420
Narthe <sismikkk@hotmail.fr>
2014-01-10 18:34:11 +0100

    Ajout de l'interface

--- Assets/scripts/GUIBehaviour.cs
+++ Assets/scripts/GUIBehaviour.cs
@@ -3,20 +3,35 @@ using System.Collections;
 
 public class GUIBehaviour : MonoBehaviour
 {
-	public Rect labelPosition;
-	string labelText;
-	public GUIStyle labelStyle;
-	int score = 0;
-	
-	//Set labelPosition public so it can be changed in inspector (editor). 
-	
+	public Rect scorePosition;
+	public Rect goldPosition;
+	public Rect dataLeftPosition;
+	public Rect goldSpritePosition;
+	public Rect sloppySpritePosition;
+
+	public GUIStyle Style;
+	string score;
+	string gold = "5000";
+	string dataLeft = "253";
+
+	string globalScore = "100";
+
+	public Texture2D goldSprite;
+	public Texture2D sloppyDiskSprite;
+
 	void Update()
 	{
-		labelText = "100";
+		score = (string)globalScore;
 	}
 	
 	void OnGUI()
 	{
-		GUI.Label(labelPosition, labelText, labelStyle);
+		GUI.Label(scorePosition, score, Style);
+		GUI.Label(goldPosition, gold, Style);
+		GUI.Label(dataLeftPosition, dataLeft, Style);
+
+		GUI.DrawTexture(goldSpritePosition, goldSprite, ScaleMode.ScaleToFit, true, 0.0F);
+		GUI.DrawTexture(sloppySpritePosition, sloppyDiskSprite, ScaleMode.ScaleToFit, true, 0.0F);
+
 	}
 }
